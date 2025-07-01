78be49cd689211f65caf51cae842c913b402f48b
Oskar Richter <netmail678@gmail.com>
2025-07-01 22:29:47 +0200

    implement basic methods (linkedlist)

--- LinkedList/linkedList.h
+++ LinkedList/linkedList.h
@@ -1,12 +1,12 @@
 typedef struct LinkedList LinkedList;
 typedef struct LinkedListNode LinkedListNode;
 
-LinkedList *LinkedListCreate();
-void* linkedListGet(LinkedList *pList, int index);
-void linkedListSet(LinkedList *pList,void *value, int index);
+LinkedList *LinkedListCreate(int size);
+void *linkedListGet(LinkedList *pList, int index);
+void linkedListSet(LinkedList *pList, void *value, int index);
 void linkedListAdd(LinkedList *pList, void *value);
 void linkedListAddIndex(LinkedList *pList, void *value, int index);
 void linkedListRemove(LinkedList *pList, int index);
 int linkedListLength(LinkedList *pList);
-
+int linkedListSize(LinkedList *pList);
 void listFree(LinkedList *pList);
