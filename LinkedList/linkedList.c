b833183c7d5b0ca116a3add0a439d5df028d0a9f
Oskar Richter <netmail678@gmail.com>
2025-07-01 22:29:01 +0200

    implement basic methods (linkedlist)

--- LinkedList/linkedList.c
+++ LinkedList/linkedList.c
@@ -1,116 +1,158 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "linkedList.h"
 
-typedef struct LinkedList{
+LinkedListNode *linkedListGetNode(LinkedList *pList, int index);
+void freeNode(LinkedListNode *pNode);
+
+typedef struct LinkedList
+{
     LinkedListNode *head;
     int length;
+    int size;
 } LinkedList;
 
-typedef struct LinkedListNode{
+typedef struct LinkedListNode
+{
     LinkedListNode *next;
     void *value;
 } LinkedListNode;
 
-LinkedList *LinkedListCreate()
+LinkedList *LinkedListCreate(int size)
 {
-    LinkedList *pList = (LinkedList*) malloc(sizeof(LinkedList));
-    
-    if(pList == NULL){
+    LinkedList *pList = (LinkedList *)malloc(sizeof(LinkedList));
+
+    if (pList == NULL)
+    {
         printf("Memory allocation failed!");
         return NULL;
     }
 
     pList->head = NULL;
     pList->length = 0;
+    pList->size = size;
 
     return pList;
 }
 
 void *linkedListGet(LinkedList *pList, int index)
 {
-    if(index >= pList->length || index < 0){
+    if (index >= pList->length || index < 0)
+    {
         printf("Index out of bounds!");
         return NULL;
     }
 
-    LinkedListNode *pNode = pList->head;
+    LinkedListNode *pNode = linkedListGetNode(pList, index);
 
-    for(int i = 0; i < index; i++){
-        pNode = pNode->next;
-    }
     return pNode->value;
 }
 
 void linkedListSet(LinkedList *pList, void *value, int index)
 {
-    if(index >= pList->length || index < 0){
+    if (index >= pList->length || index < 0)
+    {
         printf("Index out of bounds!");
         return NULL;
     }
 
-    LinkedListNode *pNode = pList->head;
+    void *pValue = linkedListGet(pList, index);
+    free(pValue);
+    pValue = NULL;
 
-    for(int i = 0; i < index; i++){
-        pNode = pNode->next;
+    void *cp = (void *)malloc(sizeof(void *));
+
+    if (cp == NULL)
+    {
+        printf("Memory allocation failed!");
+        return NULL;
     }
-    pNode->value = value;
+    memcpy(cp, value, pList->size);
+
+    pValue = cp;
 }
 
 void linkedListAdd(LinkedList *pList, void *value)
 {
-    LinkedListNode *pNodeNew = (LinkedListNode*) malloc(sizeof(LinkedListNode));
-    if(pNodeNew == NULL){
+    LinkedListNode *pNodeNew = (LinkedListNode *)malloc(sizeof(LinkedListNode));
+    if (pNodeNew == NULL)
+    {
         printf("Memory allocation failed!");
         return NULL;
     }
 
-    LinkedListNode *pNode = pList->head;
+    void *cp = (void *)malloc(sizeof(void *));
 
-    while(pNode->next != NULL){
-        pNode = pNode->next;
+    if (cp == NULL)
+    {
+        printf("Memory allocation failed!");
+        return NULL;
     }
-    pNodeNew->value = value;
+    memcpy(cp, value, pList->size);
+
+    pNodeNew->value = cp;
     pNodeNew->next = NULL;
-    pNode->next = pNodeNew;
+
+    if (pList->length == 0)
+    {
+        pList->head = pNodeNew;
+    }
+    else
+    {
+        LinkedListNode *node = linkedListGetNode(pList, pList->length - 1);
+
+        node->next = pNodeNew;
+    }
 
     pList->length++;
 }
 
 void linkedListAddIndex(LinkedList *pList, void *value, int index)
 {
-    if(index >= pList->length || index < 0){
+    if (index >= pList->length || index < 0)
+    {
         printf("Index out of bounds!");
         return NULL;
     }
 
-    LinkedListNode *pNodeNew = (LinkedListNode*) malloc(sizeof(LinkedListNode));
-    if(pNodeNew == NULL){
+    LinkedListNode *pNodeNew = (LinkedListNode *)malloc(sizeof(LinkedListNode));
+    if (pNodeNew == NULL)
+    {
         printf("Memory allocation failed!");
         return NULL;
     }
 
-    LinkedListNode *pNodeP = NULL;
-    LinkedListNode *pNodeC = pList->head;
+    void *cp = (void *)malloc(sizeof(void *));
 
-    for(int i = 0; i < index; i++){
-        if(pNodeP == NULL){
-            pNodeP = pList->head;
-        }
-        else{
-            pNodeP = pNodeP->next;
-        }
-        pNodeC = pNodeC->next;
+    if (cp == NULL)
+    {
+        printf("Memory allocation failed!");
+        return NULL;
     }
+    memcpy(cp, value, pList->size);
 
-    pNodeNew->value = value;
-    pNodeNew->next = pNodeC;
+    pNodeNew->value = cp;
+    pNodeNew->next = NULL;
 
-    if(pNodeP == NULL){
+    LinkedListNode *pNodeP;
+    LinkedListNode *pNodeC;
+
+    if (index == 0)
+    {
+        pNodeP = NULL;
+        pNodeC = pList->head,
+
         pList->head = pNodeNew;
+        pNodeNew->next = pNodeC;
     }
-    else{
+    else
+    {
+        pNodeP = linkedListGet(pList, index - 1);
+        pNodeC = pNodeP->next;
+
         pNodeP->next = pNodeNew;
+        pNodeNew->next = pNodeC;
     }
 
     pList->length++;
@@ -118,34 +160,32 @@ void linkedListAddIndex(LinkedList *pList, void *value, int index)
 
 void linkedListRemove(LinkedList *pList, int index)
 {
-    if(index >= pList->length || index < 0){
+    if (index >= pList->length || index < 0)
+    {
         printf("Index out of bounds!");
         return NULL;
     }
 
-    LinkedListNode *pNodeP = NULL;
-    LinkedListNode *pNodeC = pList->head;
+    LinkedListNode *pNodeP;
+    LinkedListNode *pNodeC;
 
-    for(int i = 0; i < index; i++){
-        if(pNodeP == NULL){
-            pNodeP = pList->head;
-        }
-        else{
-            pNodeP = pNodeP->next;
-        }
-        pNodeC = pNodeC->next;
+    if (index == 0)
+    {
+        pNodeP = NULL;
+        pNodeC = pList->head->next;
+
+        freeNode(pList->head);
+        pList->head = pNodeC;
+    }
+    else
+    {
+        pNodeP = linkedListGetNode(pList, index - 1);
+        pNodeC = pNodeP->next->next;
+
+        freeNode(pNodeP->next);
+        pNodeP->next = pNodeC;
     }
 
-    if(pNodeP == NULL){
-        pList->head = pNodeC->next;
-    }
-    else if(pNodeC->next == NULL){
-        pNodeP->next = NULL;
-    }
-    else{
-        pNodeP->next = pNodeC->next;
-    }
-    free(pNodeC);
     pList->length--;
 }
 
@@ -154,6 +194,54 @@ int linkedListLength(LinkedList *pList)
     return pList->length;
 }
 
+int linkedListSize(LinkedList *pList)
+{
+    return pList->size;
+}
+
 void listFree(LinkedList *pList)
 {
+    LinkedListNode *pNode = pList->head;
+
+    for (int i = 0; i < pList->length; i++)
+    {
+        LinkedListNode *pNodeTemp = pNode->next;
+
+        freeNode(pNode);
+        pNode = pNodeTemp;
+    }
+
+    free(pList);
+}
+
+LinkedListNode *linkedListGetNode(LinkedList *pList, int index)
+{
+    if (index >= pList->length || index < 0)
+    {
+        printf("Index out of bounds!");
+        return NULL;
+    }
+
+    if (pList->length == 0)
+    {
+        printf("Can not get a node from an empty list!");
+        return NULL;
+    }
+
+    LinkedListNode *node = pList->head;
+
+    for (int i = 0; i < index; i++)
+    {
+        node = node->next;
+    }
+
+    return node;
+}
+
+void freeNode(LinkedListNode *pNode)
+{
+    free(pNode->value);
+    free(pNode);
+
+    pNode = NULL;
 }
