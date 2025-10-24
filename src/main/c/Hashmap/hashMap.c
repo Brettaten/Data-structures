f26d4bb89b459737f5c8da4aa0a075e90ff903a7
Oskar Richter <netmail678@gmail.com>
2025-10-24 18:43:16 +0200

    add basic hash map functions

--- src/main/c/Hashmap/hashMap.c
+++ src/main/c/Hashmap/hashMap.c
@@ -1,40 +1,245 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 #include "hashMap.h"
+#include "../List/list.h"
+#include "../SinglyLinkedList/singlyLinkedList.h"
+
+typedef struct HashMap
+{
+    List *list;
+    int length;
+    int size;
+    int sizeKey;
+    int sizeValue;
+    void *(*copyKey)(void *);
+    void (*freeKey)(void *);
+    void *(*copyValue)(void *);
+    void (*freeValue)(void *);
+    int64_t *(*hash)(void *, int);
+    bool *(*equals)(void *, void *, int);
+} HashMap;
+
+/**
+ * Struct that represents an entry of a hashmap
+ */
+typedef struct Node
+{
+    void *key;
+    void *value;
+    int64_t hash;
+} Node;
 
 /**
  * Function used to hash any pointer to a 64 bit integer
- * 
+ *
  * @param value pointer to the value
  * @param size the size that value holds
- * 
+ *
  * @return Success: a 64 bit hash | Failure: NULL
  */
 int64_t hash(void *value, int size);
 
+/**
+ * Function used to test if two variables store the same bits
+ *
+ * @param value1 pointer to the first value
+ * @param value2 pointer to the second value
+ * @param size the size of both pointers
+ *
+ * @return Success: true | Failure: false
+ */
+bool equals(void *value1, void *value2, int size);
+
+/**
+ * Funcion used to create a deep copy of the passed Node
+ *
+ * @param pMap the pointer to the hash map
+ * @param pNode the pointer to the node
+ *
+ * @return Success: the deep copy | Failure: NULL
+ */
+Node *hashMapCopyNode(HashMap *pMap, Node *pNode);
+
+/**
+ * Function used to free the passed Node
+ *
+ * @param pMap the pointer to the hash map
+ * @param pNode the pointer to the node
+ */
+void hashMapFreeNode(HashMap *pMap, Node *pNode);
+
+HashMap *hashMapCreate(int sizeKey, void *(*copyKey)(void *), void (*freeKey)(void *), int sizeValue, void *(*copyValue)(void *), void (*freeValue)(void *), int64_t *(*hash)(void *, int), bool *(*equals)(void *, void *, int))
+{
+    HashMap *pMap = (HashMap *)malloc(sizeof(HashMap));
+
+    if (pMap == NULL)
+    {
+        printf("[ERROR] : Memory allocation failed | hashMapCreate \n");
+        return NULL;
+    }
+
+    List *pList = listCreate(sizeof(singlyLinkedListSize()), &singlyLinkedListCopy, &singlyLinkedListFree);
+
+    if (pList == NULL)
+    {
+        printf("[ERROR] : Memory allocation failed | hashmapCreate \n");
+        return NULL;
+    }
+
+    int initalSize = 16;
+
+    for (int i = 0; i < initalSize; i++)
+    {
+        SinglyLinkedList *linkedList = singlyLinkedListCreate(sizeof(Node), &hashMapCopyNode, &hashMapFreeNode);
+
+        if (linkedList == NULL)
+        {
+            printf("[ERROR] : Memory allocation failed | hashMapCreate \n");
+        }
+
+        listAdd(pList, linkedList);
+    }
+
+    pMap->list = pList;
+    pMap->length = initalSize;
+    pMap->size = 0;
+    pMap->sizeKey = sizeKey;
+    pMap->sizeValue = sizeValue;
+    pMap->copyKey = copyKey;
+    pMap->freeKey = freeKey;
+    pMap->copyValue = copyValue;
+    pMap->freeValue = freeValue;
+    pMap->hash = hash;
+    pMap->equals = equals;
+
+    return pMap;
+}
+
+int hashMapLength(HashMap *pMap)
+{
+    if (pMap == NULL)
+    {
+        printf("[ERROR] : pMap is null | hashMapLength \n");
+        return -1;
+    }
+
+    return pMap->size;
+}
+
+int hashMapKeySize(HashMap *pMap)
+{
+    if (pMap == NULL)
+    {
+        printf("[ERROR] : pMap is null | hashMapKeySize \n");
+        return -1;
+    }
+
+    return pMap->sizeKey;
+}
+
+int hashMapValueSize(HashMap *pMap)
+{
+    if (pMap == NULL)
+    {
+        printf("[ERROR] : pMap is null | hashMapValueSize \n");
+        return -1;
+    }
+
+    return pMap->sizeValue;
+}
+
+int hashMapSize()
+{
+    return sizeof(HashMap);
+}
+
+void *hashMapCopy(void *pMap)
+{
+    HashMap *cp = (HashMap *)pMap;
+
+    if (cp == NULL)
+    {
+        printf("[ERROR] : HashMap is null | hashMapCopy \n");
+        return NULL;
+    }
+
+    HashMap *copy = (HashMap *)malloc(sizeof(HashMap));
+
+    if (copy == NULL)
+    {
+        printf("[ERROR] : Memory allocation failed | hashMapCopy \n");
+        return NULL;
+    }
+
+    List *pListCopy = listCopy(cp->list);
+
+    if (pListCopy == NULL)
+    {
+        printf("[ERROR] : Memory allocation failed | hashMapCopy \n");
+    }
+
+    copy->list = pListCopy;
+    copy->length = cp->length;
+    copy->size = cp->size;
+    copy->sizeKey = cp->sizeKey;
+    copy->sizeValue = cp->sizeValue;
+    copy->copyKey = cp->copyKey;
+    copy->freeKey = cp->freeKey;
+    copy->copyValue = cp->copyValue;
+    copy->freeValue = cp->freeValue;
+    copy->hash = cp->hash;
+    copy->equals = cp->equals;
+
+    return copy;
+}
+
+void hashMapFree(void *pMap)
+{
+    HashMap *cp = (HashMap *)pMap;
+
+    if (cp == NULL)
+    {
+        printf("[WARN] : Pointer to hash map is NULL | hashMapFree \n");
+        return;
+    }
+
+    listFree(cp->list);
+
+    free(cp);
+
+    pMap = NULL;
+}
+
 int64_t hash(void *value, int size)
 {
-    if(value == NULL){
+    if (value == NULL)
+    {
         printf("[ERROR] : value is null | hash \n");
         return NULL;
     }
 
     int64_t initialHash = 4366080005767977372;
 
-    int iterations = (size/8) + 1;
+    int iterations = (size / 8) + 1;
     int pos = 0;
-    int8_t *pValue = (int*)value;
+    int8_t *pValue = (int *)value;
 
-    for(int i = 0; i < iterations && i < 16; i++){
+    for (int i = 0; i < iterations && i < 16; i++)
+    {
         int chunkSize = 8;
-        if(size - pos < 8){
+        if (size - pos < 8)
+        {
             chunkSize = size - pos;
         }
 
         int64_t chunkValue = 0;
 
-        for(int j = 0; j < chunkSize; j++){
+        for (int j = 0; j < chunkSize; j++)
+        {
             chunkValue |= ((int64_t)pValue[pos + j]) << (8 * j);
         }
 
@@ -42,10 +247,142 @@ int64_t hash(void *value, int size)
 
         pos += chunkSize;
 
-        if(pos >= size){
+        if (pos >= size)
+        {
             break;
         }
     }
 
     return initialHash;
+}
+
+bool equals(void *value1, void *value2, int size)
+{
+    if (value1 == NULL || value2 == NULL)
+    {
+        printf("[ERROR] : value is null | equals \n");
+        return false;
+    }
+
+    if(memcmp(value1, value2, size) == 0){
+        return true;
+    }
+    return false;
+}
+
+Node *hashMapCopyNode(HashMap *pMap, Node *pNode)
+{
+    if (pNode == NULL)
+    {
+        printf("[ERROR] : pNode is null | hashMapCopyNode \n");
+        return NULL;
+    }
+
+    if (pMap == NULL)
+    {
+        printf("[ERROR] : pMap is null | hashMapCopyNode \n");
+        return NULL;
+    }
+
+    Node *cp = (Node *)malloc(sizeof(Node));
+
+    if (cp == NULL)
+    {
+        printf("[ERROR] : Memory allocation failed | hashMapCopyNode \n");
+        return NULL;
+    }
+
+    void *cK;
+    void *cV;
+
+    if (pMap->copyKey == NULL)
+    {
+        cK = (void *)malloc(pMap->sizeKey);
+
+        if (cK == NULL)
+        {
+            printf("[ERROR] : Memory allocation failed | hashMapCopyNode \n");
+            return NULL;
+        }
+
+        memcpy(cK, pNode->key, pMap->sizeKey);
+    }
+    else
+    {
+        cK = pMap->copyKey(pNode->key);
+
+        if (cK == NULL)
+        {
+            printf("[ERROR] : Memory allocation failed | hashMapCopyNode \n");
+            return NULL;
+        }
+    }
+
+    if (pMap->copyValue == NULL)
+    {
+        cV = (void *)malloc(pMap->sizeValue);
+
+        if (cV == NULL)
+        {
+            printf("[ERROR] : Memory allocation failed | hashMapCopyNode \n");
+            return NULL;
+        }
+
+        memcpy(cV, pNode->value, pMap->sizeValue);
+    }
+    else
+    {
+        cV = pMap->copyValue(pNode->value);
+
+        if (cV == NULL)
+        {
+            printf("[ERROR] : Memory allocation failed | hashMapCopyNode \n");
+            return NULL;
+        }
+    }
+
+    cp->key = cK;
+    cp->value = cV;
+    cp->hash = pNode->hash;
+
+    return cp;
+}
+
+void hashMapFreeNode(HashMap *pMap, Node *pNode)
+{
+    if (pNode == NULL)
+    {
+        printf("[ERROR] : pNode is null | hashMapCopyNode \n");
+        return NULL;
+    }
+
+    if (pMap == NULL)
+    {
+        printf("[ERROR] : pMap is null | hashMapCopyNode \n");
+        return NULL;
+    }
+
+    if (pMap->freeKey == NULL)
+    {
+        free(pNode->key);
+    }
+    else
+    {
+        pMap->freeKey(pNode->key);
+    }
+    if (pMap->freeValue == NULL)
+    {
+        free(pMap->freeValue);
+    }
+    else
+    {
+        pMap->freeValue(pNode->value);
+    }
+
+    pNode->key = NULL;
+    pNode->value = NULL;
+
+    free(pNode);
+
+    pNode = NULL;
 }
---
+++ src/main/c/Hashmap/hashMap.h
@@ -0,0 +1,131 @@
+/**
+ * Struct that represents a hash map
+ *
+ * @note Only use this hash map to store data that has the same size as specified.
+ * @note Only store one data type in each hash map to be safe.
+ */
+typedef struct HashMap HashMap;
+
+/**
+ * Function that allocates memory for a hash map struct
+ *
+ * @param sizeKey the size of the keys that will be stored in the hash map
+ * @param copyKey pointer to a function that deep copies the key, pass NULL for elements, where a shallow copy is sufficient
+ * @param freeKey pointer to a function that deep frees the key, pass NULL for elements, where a shallow free is sufficient
+ * @param sizeValue the size of the values that will be stored in the hash map
+ * @param copyValue pointer to a function that deep copies the value, pass NULL for elements, where a shallow copy is sufficient
+ * @param freeValue pointer to a function that deep frees the value, pass NULL for elements, where a shallow free is sufficient
+ * @param hash Pointer to a function that hashes the key, pass NULL for keys, where a shallow hash is sufficient
+ * @param equals Pointer to a function that compares the keys, pass NULL for keys, where a shallow compare is sufficient
+ *
+ * @return Success: Pointer to the created hash map | Failure: NULL
+ */
+HashMap *hashMapCreate(int sizeKey, void *(*copyKey)(void *), void (*freeKey)(void *), int sizeValue, void *(*copyValue)(void *), void (*freeValue)(void *), int64_t *(*hash)(void *, int), bool *(*equals)(void *, void *, int));
+
+/**
+ * Function that retrieves the element of the hash map with the passed key
+ *
+ * @param pMap pointer to the hash map
+ * @param key the key
+ *
+ * @return Success: Pointer to the element of the passed key | Failure: NULL
+ *
+ * @note The returned pointer is a deep copy and thus, has to be freed by the caller
+ */
+void *hashMapGet(HashMap *pMap, void *key);
+
+/**
+ * Function that sets the value of the element that is associated with the passed key
+ *
+ * @param pMap pointer to the hash map
+ * @param value pointer to the element that should replace the old element
+ * @param index the key
+ *
+ * @return Success: 0 | Failure: -1
+ *
+ * @note The passed value does not have to be allocated on the heap.
+ * The function creates a deep copy of the value before adding it to the hash map.
+ */
+int hashMapSet(HashMap *pMap, void *value, void *key);
+
+/**
+ * Function used to add the passed value to the hash map
+ *
+ * @param pMap pointer to the hash map
+ * @param value pointer to a value
+ * @param key the key
+ *
+ * @return Success: 0 | Failure: -1
+ *
+ * @note The passed value does not have to be allocated on the heap.
+ * The function creates a deep copy of the value before adding it to the hash map.
+ */
+int hashMapAdd(HashMap *pMap, void *value, void *key);
+
+/**
+ * Function used to remove the data that is associated with the passed key
+ *
+ * @param pMap pointer to the hash map
+ * @param key the key
+ *
+ * @return Success: 0 | Failure: -1
+ */
+int hashMapRemove(HashMap *pMap, void *key);
+
+/**
+ * Function used to return the length of the hash map
+ *
+ * @param pMap pointer to the hash map
+ *
+ * @return Success: length | Failure: -1
+ */
+int hashMapLength(HashMap *pMap);
+
+/**
+ * Function used to return the size of the key
+ *
+ * @param pMap pointer to the hash map
+ *
+ * @return Success: size | Failure: -1
+ */
+int hashMapKeySize(HashMap *pMap);
+
+/**
+ * Function used to return the size of the value
+ *
+ * @param pMap pointer to the hash map
+ *
+ * @return Success: size | Failure: -1
+ */
+int hashMapValueSize(HashMap *pMap);
+
+/**
+ * Function used to return the size of a hash map struct
+ *
+ * @return Success: size
+ */
+int hashMapSize();
+
+/**
+ * Function used to create a deep copy of a hash map
+ *
+ * @param pMap the pointer to the hash map
+ *
+ * @return Success: the copy of the hash map | Failure: NULL
+ *
+ * @note Void datatypes are used for this function because one might
+ *      store this struct inside this struct and thus function pointers
+ *      with void datatype are needed
+ */
+void *hashMapCopy(void *pMap);
+
+/**
+ * Function used to free the passed hash map
+ *
+ * @param pList pointer to the hash map
+ * 
+ * @note Void datatypes are used for this function because one might
+ *      store this struct inside this struct and thus function pointers
+ *      with void datatype are needed
+ */
+void hashMapFree(void *pMap);
